f364957a2c743c9cff1f15838172db4b3b0c5f64
EthBeats <ethbeats3@gmail.com>
2023-11-19 17:07:06 -0500

    GUI listens to param changes and refactor low/high cut

--- Source/PluginProcessor.cpp
+++ Source/PluginProcessor.cpp
@@ -239,7 +239,7 @@ void updateCoefficients(Coefficients &old, const Coefficients &replacements) {
 }
 
 void SimpleEQAudioProcessor::updateLowCutFilters(const ChainSettings &chainSettings) {
-	auto lowCutCoefficients = juce::dsp::FilterDesign<float>::designIIRHighpassHighOrderButterworthMethod(chainSettings.lowCutFreq, getSampleRate(), (chainSettings.lowCutSlope + 1) * 2);
+	auto lowCutCoefficients = makeLowCutFilter(chainSettings, getSampleRate());
     
     auto &leftLowCut = leftChain.get<ChainPositions::LowCut>();
 	auto &rightLowCut = rightChain.get<ChainPositions::LowCut>();
@@ -249,7 +249,7 @@ void SimpleEQAudioProcessor::updateLowCutFilters(const ChainSettings &chainSetti
 }
 
 void SimpleEQAudioProcessor::updateHighCutFilters(const ChainSettings &chainSettings) {
-	auto highCutCoefficients = juce::dsp::FilterDesign<float>::designIIRLowpassHighOrderButterworthMethod(chainSettings.highCutFreq, getSampleRate(), (chainSettings.highCutSlope + 1) * 2);
+	auto highCutCoefficients = makeHighCutFilter(chainSettings, getSampleRate());
 	
 	auto &leftHighCut = leftChain.get<ChainPositions::HighCut>();
 	auto &rightHighCut = rightChain.get<ChainPositions::HighCut>();
